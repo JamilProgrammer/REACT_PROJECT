5d34922c11f3b015e164d5dc3111979605d87572
JamilProgrammer <pinklovermohamed@gmail.com>
2022-01-29 15:49:57 +0530

    workout

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Selecao from "./Selecao.js";
 // import Todo from "./todo.js";
 // import Sel1 from "./sel1.js";
-import Todo1 from "./todo1.js";
+// import Todo1 from "./todo1.js";
 function App() {
   return (
     <div>
+      <Selecao />
       {/*<Textbox/>*/}
       {/*<Sel1 />*/}
-      <Todo1 />
+      {/* <Todo1 /> */}
       {/* <Mujamil name="username" age="99"/> */}
     </div>
   );
